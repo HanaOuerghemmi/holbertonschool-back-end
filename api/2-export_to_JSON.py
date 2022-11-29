5ad194a59008ff01b4f835588fdcedaf607d04db
HanaOuerghemmi <5004@holbertonschool.com>
2022-11-29 17:04:47 +0100

    export json file

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Returns to-do list information for a given employee ID."""
+import json
+
 import requests
 import sys
-import json
 
 if __name__ == "__main__":
     api_url = "https://jsonplaceholder.typicode.com/"
@@ -10,10 +11,10 @@ if __name__ == "__main__":
     todos = requests.get(
         api_url + "todos", params={"userId": sys.argv[1]}).json()
 
-    dicti = {str(user.get("id")) : [{"task": task.get("title"), 
-                                "completed" : task.get("completed"),
-                                "username" : user.get("username")}for task in todos]}
-                
+    dicti = {str(user.get("id")): [{"task": task.get("title"),
+                                    "completed": task.get("completed"),
+                                    "username": user.get("username")}for task in todos]}
+
     file_json = sys.argv[1] + ".json"
     with open(file_json, "w") as f:
 
